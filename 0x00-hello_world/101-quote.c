5376c78e18111f52d4f17114013c88cb1690c454
Mutai-Gilbert <mutaigilbert85@gmail.com>
2022-06-30 12:07:41 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include <unistd.h>
+
+
+
+/**
+ *
+ *  * main - prints exactly "and that piece of art is useful"
+ *
+ *   * - Gideon Obae, 2021-08-26",
+ *
+ *    * followed by a new line, to the standard error.
+ *
+ *     * Return: 1 if success.
+ *
+ *      */
+
+int main(void)
+
+{
+
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+			return (1);
+
+}
