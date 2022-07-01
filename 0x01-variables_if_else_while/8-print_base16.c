fb2389faa10c3884ab7ad68e94c241e5f8fadbe8
Mutai-Gilbert <mutaigilbert85@gmail.com>
2022-07-01 14:31:33 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  *main - Entry point
+ *
+ *   *Description - Printing base 16 numbers
+ *
+ *    *Return:0
+ *
+ *     */
+
+int main(void)
+
+{
+	int i, y;
+	for (i = 0; i < 10; i++)
+	    {
+		    putchar (i + '0');
+    	    }       
+	for (y = 'a'; y <= 'f'; y++)
+            {
+	       	    putchar (y);
+             }	   
+        putchar('\n');
+        return (0);	   
+}
