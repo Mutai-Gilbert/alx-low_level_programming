60e741b7b695213358c40ec1a0a2dfb360c7f938
Mutai-Gilbert <mutaigilbert85@gmail.com>
2022-07-01 00:39:30 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   * Description: prints "\"Programming is like building a multilingual puzzle"
- *    * Return: 0
- *     */
-
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ * /
+ 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
