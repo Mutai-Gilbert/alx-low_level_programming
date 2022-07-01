67d19397e8c6de37517a503d864526eafd1bfed8
Mutai-Gilbert <mutaigilbert85@gmail.com>
2022-07-01 01:46:20 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,22 +5,18 @@
 
 
 /**
- *
  *  * main - prints exactly "and that piece of art is useful"
- *
  *   * - Gideon Obae, 2021-08-26",
- *
  *    * followed by a new line, to the standard error.
- *
  *     * Return: 1 if success.
- *
- *      */
+ */
 
 int main(void)
 
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		write(2, "and that piece of art i
+				\"s useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 			return (1);
 
