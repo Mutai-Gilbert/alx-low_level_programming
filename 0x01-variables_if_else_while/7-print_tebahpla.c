8402d223aee3a33256e85e2b0d387c6b04ca2a49
Mutai-Gilbert <mutaigilbert85@gmail.com>
2022-07-02 09:40:20 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -14,12 +14,12 @@
 int main(void)
 
 {
-	char c = 'z';
-	while (c >= 'a')
-	{
-	putchar(c);
-	c--;
-	}
-	putchar('\n');
-	return (0);
+char c = 'z';
+while (c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
 }
