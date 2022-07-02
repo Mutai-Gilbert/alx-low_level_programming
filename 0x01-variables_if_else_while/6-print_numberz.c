a5f3a577659793a104234c328dc42a619ded0b27
Mutai-Gilbert <mutaigilbert85@gmail.com>
2022-07-02 09:36:35 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,12 +10,12 @@
 int main(void)
 
 {
-	int c = 0;
-	while (c < 10)
-	{
-	putchar(48 + c);
-	c++;
-	}
-	putchar('\n');
-	return (0);
+int c = 0;
+while (c < 10)
+{
+putchar(48 + c);
+c++;
+}
+putchar('\n');
+return (0);
 }
