bbb37609b99e159d75770427bb4db17f7070f7d0
Mutai-Gilbert <mutaigilbert85@gmail.com>
2022-07-05 20:09:21 +0300

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Description: The program should return 0
+ * Return: 0
+ */
+int main(void)
+{
+char gil[] = "_putchar";
+int i = 0;
+while (gil[i] != '\0')
+{
+_putchar(gil[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
