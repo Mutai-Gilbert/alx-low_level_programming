3a66db73aa6dc225f6441e249bd020e96e380e55
Mutai-Gilbert <mutaigilbert85@gmail.com>
2022-07-07 20:40:24 +0000

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  * print_most_numbers - prints 0-9 excluding 2 and 4
+ *   * Description: use _putchar twice
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+	if (i != 2 && i != 4)
+	_putchar(i + '0');
+	i++;
+	}
+	_putchar ('\n');
+}
