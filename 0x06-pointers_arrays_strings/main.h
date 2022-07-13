d9b71099263293ce8309e7465fdf8f5f87aafdcc
Mutai-Gilbert <mutaigilbert85@gmail.com>
2022-07-13 16:07:17 +0000

    Task 2

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,4 +3,6 @@
 #define MAIN_H
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 #endif /*  MAIN_H */
