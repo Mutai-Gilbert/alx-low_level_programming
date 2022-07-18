544f93ad33359283ed5bfda1a83464e37e0860ed
Mutai-Gilbert <mutaigilbert85@gmail.com>
2022-07-18 17:55:11 +0000

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - fxn copy memory area
+ * @dest: char array to copy into
+ * @src: char array to copy from
+ * @n: number of elements to copy
+ * Return: pointer to `dest`
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *yolo;
+
+	yolo = dest;
+	while (n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	return (yolo);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@
 #define MAIN_H
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif /*  MAIN_H */
