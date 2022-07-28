3e404120ddb86cd5ca96b54cb4fd22ec46eafc5e
Mutai-Gilbert <mutaigilbert85@gmail.com>
2022-07-28 18:42:46 +0000

    Task 1

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,4 +2,6 @@
 #define MAIN_H
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 #endif /*MAIN_H*/
